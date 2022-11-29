002ea3c0173d2f84012aa88a1513dfda83e7df92
jamesyusuf36 <jamesyusuf36@gmail.com>
2022-11-29 20:51:25 +0300

    Delete function is implemented.

--- Binary Tree/main.c
+++ Binary Tree/main.c
@@ -22,23 +22,28 @@ void postOrder(TreeNodePtr root);
 void preOrder(TreeNodePtr root);
 void inOrder(TreeNodePtr root);
 void insertNode(TreeNodePtr *root, int value);
-
+int findMin(TreeNodePtr root);
+int findMax(TreeNodePtr root);
+int find(TreeNodePtr root, int value);
+void delete(TreeNodePtr *root, int value);
 
 
 int main() {
 
     TreeNodePtr rootPtr = NULL;
 
-    srand(time(NULL));
+    insertNode(&rootPtr, 45);
+    insertNode(&rootPtr, 13);
+    insertNode(&rootPtr, 24);
+    insertNode(&rootPtr, 56);
+    insertNode(&rootPtr, 78);
+    insertNode(&rootPtr, 5);
+    insertNode(&rootPtr, 8);
+    insertNode(&rootPtr, 17);
 
-    //Add ten numbers between 0 and 100
-    printf("Numbers: ");
-    for(int i = 0; i < 7; i++){
-        int randomm = rand() % 100;
-        printf("%d ", randomm);
-        insertNode(&rootPtr, randomm);
-    }
-    printf("\n\n");
+
+
+
 
     puts("The preorder traversal is:");
     preOrder(rootPtr);
@@ -51,6 +56,26 @@ int main() {
     puts("The postorder traversal is:");
     postOrder(rootPtr);
     printf("\n\n");
+
+    printf("Min number in the tree is: %d\n", findMin(rootPtr));
+    printf("Max number in the tree is %d\n\n", findMax(rootPtr));
+
+    delete(&rootPtr, 13);
+    puts("After deleting 56:");
+    inOrder(rootPtr);
+    printf("\n\n");
+
+    delete(&rootPtr, 5);
+    puts("After deleting 5:");
+    inOrder(rootPtr);
+    printf("\n\n");
+
+    delete(&rootPtr, 78);
+    puts("After deleting 78:");
+    inOrder(rootPtr);
+    printf("\n\n");
+
+
 
     return 0;
 }
@@ -110,6 +135,83 @@ void postOrder(TreeNodePtr root){
         postOrder(root->rightPtr);
         printf("%d ", root->data);
     }
+}
+
+int findMin(TreeNodePtr root){
+    if(root == NULL){
+        return NULL;
+    }
+
+    while(root->leftPtr != NULL){
+        root = root->leftPtr;
+    }
+    return root->data;
+}
+
+int findMax(TreeNodePtr root){
+    if(root == NULL){
+        return NULL;
+    }
+
+    while(root->rightPtr != NULL){
+        root = root->rightPtr;
+    }
+    return root->data;
+}
+
+int find(TreeNodePtr root, int value){
+    if(root == NULL){
+        return 0;
+    }
+
+    if(root->data == value){
+        return 1;
+    }
+    else if(value > root->data)
+        return  1 == find(root->rightPtr, value); //If it is found in the right subtree, that means if the find function return 1 then it will be equal to 1 (1==1) that will return 1.
+    else
+        return  1 == find(root->leftPtr, value);
+
+}
+
+
+
+
+
+
+void delete(TreeNodePtr *root, int value){
+
+    if(root == NULL) //If the tree is empty return
+        return;
+
+    if((*root)->data == value){  //If the value is found
+
+        if((*root)->rightPtr == NULL && (*root)->leftPtr == NULL){ //If the current root is a leaf
+            TreeNodePtr temp = *root;
+            *root = NULL;
+            free(temp);
+            return;
+        }
+        else if((*root)->rightPtr != NULL){  //If the right child exist
+            (*root)->data = findMin((*root)->rightPtr); //Copy the min value of the right subtree to the node
+            delete(&((*root)->rightPtr), findMin((*root)->rightPtr)); //Delete the duplicate value
+            return;
+        }
+        else{ //If the left child exits
+            (*root)->data = findMin((*root)->leftPtr);
+            delete(&((*root)->leftPtr), findMin((*root)->leftPtr));
+            return;
+        }
+    }
+    else if (value > (*root)->data){
+        delete(&((*root)->rightPtr), value);
+        return;
+    }
+    else{
+        delete(&((*root)->leftPtr), value);
+        return;
+    }
+
 }
 
 
