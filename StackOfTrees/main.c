80684d70e2117c31eb2d9db15ca9b29e3fe12733
jamesyusuf36 <jamesyusuf36@gmail.com>
2022-12-04 18:21:52 +0300

    Starting

---
+++ StackOfTrees/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Author: Yusuf Demir  |   GitHub-->@ItsYusufDemir
+ * Starting date: 229.11.2022
+ *
+ * Project Description: Building a binary tree from a arithmetic expression.
+ */
+
