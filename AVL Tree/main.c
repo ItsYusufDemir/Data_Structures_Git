d892fa8dda6f2506986b6796b093dc458702828b
jamesyusuf36 <jamesyusuf36@gmail.com>
2022-12-20 19:58:18 +0300

    BST, 2D printing is working

---
+++ AVL Tree/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+/* Author: Yusuf Demir  |   GitHub-->@ItsYusufDemir
+ * Starting date: 29.11.2022
+ *
+ * Project Description: Building a binary tree
+ */
+
+
+//STRUCTURES
+struct treeNode{
+    int data;
+    unsigned int height;
+    struct treeNode *left;
+    struct treeNode *right;
+};
+typedef struct treeNode TreeNode;
+typedef TreeNode *TreeNodePtr;
+
+
+
+//FUNCTION PROTOTYPES
+
+
+
+
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
