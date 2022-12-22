f1b81c2da5e31f8625c8e6c618c7987d7d308208
jamesyusuf36 <jamesyusuf36@gmail.com>
2022-12-22 22:27:27 +0300

    AVL tree is implemented.

--- AVL Tree/main.c
+++ AVL Tree/main.c
@@ -4,9 +4,9 @@
 
 
 /* Author: Yusuf Demir  |   GitHub-->@ItsYusufDemir
- * Starting date: 29.11.2022
+ * Starting date: 20.12.2022
  *
- * Project Description: Building a binary tree
+ * Project Description: Building an AVL tree
  */
 
 
@@ -21,6 +21,8 @@ typedef struct treeNode TreeNode;
 typedef TreeNode *TreeNodePtr;
 
 
+int height = -1;
+
 
 //FUNCTION PROTOTYPES
 void insertNode(TreeNodePtr *root, int value);
@@ -33,6 +35,15 @@ int findHeight(TreeNodePtr root);
 void updateHeights(TreeNodePtr *root);
 void checkBalance(TreeNodePtr *root);
 void balanceAVL(TreeNodePtr *root);
+int getHeight(TreeNodePtr root);
+void rotateRR(TreeNodePtr *root);
+void rotateLL(TreeNodePtr *root);
+void rotateRL(TreeNodePtr *root);
+void rotateLR(TreeNodePtr *root);
+void copyTree(TreeNodePtr main, TreeNodePtr *duplicate);
+void printTree2D(TreeNodePtr tree);
+
+
 
 
 
@@ -48,26 +59,60 @@ int main() {
 
     TreeNodePtr rootPtr = NULL;
 
-    insertNode(&rootPtr, 45);
-    //checkBalance(&rootPtr); // After inserting a value, we should balance it
-    insertNode(&rootPtr, 13);
-    insertNode(&rootPtr, 24);
-    insertNode(&rootPtr, 56);
-    insertNode(&rootPtr, 78);
-    insertNode(&rootPtr, 5);
-    insertNode(&rootPtr, 8);
-    insertNode(&rootPtr, 17);
+    /*
+    insertNode(&rootPtr, 21);
+    insertNode(&rootPtr, 26);
+    insertNode(&rootPtr, 30);
     insertNode(&rootPtr, 9);
+    insertNode(&rootPtr, 4);
+    insertNode(&rootPtr, 14);
+    insertNode(&rootPtr, 28);
+    insertNode(&rootPtr, 18);
+    insertNode(&rootPtr, 15);
+    insertNode(&rootPtr, 10);
+    insertNode(&rootPtr, 2);
+    insertNode(&rootPtr, 3);
+    insertNode(&rootPtr, 7);
+    */
 
-    checkBalance(&rootPtr);
+    /*
+    insertNode(&rootPtr, 14);
+    insertNode(&rootPtr, 17);
+    insertNode(&rootPtr, 11);
+    insertNode(&rootPtr, 7);
+    insertNode(&rootPtr, 53);
+    insertNode(&rootPtr, 4);
+    insertNode(&rootPtr, 13);
+    insertNode(&rootPtr, 12);
+    insertNode(&rootPtr, 8);
+    insertNode(&rootPtr, 60);
+    insertNode(&rootPtr, 19);
+    insertNode(&rootPtr, 16);
+    insertNode(&rootPtr, 20);
+*/
+
+    insertNode(&rootPtr, 9);
+    insertNode(&rootPtr, 15);
+    insertNode(&rootPtr, 20);
+    insertNode(&rootPtr, 8);
+    insertNode(&rootPtr, 7);
+    insertNode(&rootPtr, 13);
+    insertNode(&rootPtr, 10);
 
 
 
 
-
-    puts("The inorder traversal is:");
-    inOrder(rootPtr);
+    printTree2D(rootPtr);
     printf("\n\n");
+
+
+    //checkBalance(&rootPtr);
+
+
+
+
+
+
 
 
 
@@ -107,6 +152,7 @@ void insertNode(TreeNodePtr *root, int value){
     }
 
     updateHeights(root); //After we insert a node, we update all the heights of the nodes in the tree
+    checkBalance(root);
 }
 
 int findMax(TreeNodePtr root){
@@ -217,18 +263,8 @@ void checkBalance(TreeNodePtr *root){ //We check each node in the tree recursive
     checkBalance(&((*root)->left));
     checkBalance(&((*root)->right));
 
-    if((*root)->right == NULL && (*root)->left == NULL) //If the current root is a leaf, there is nothing to check
-        return;
-    else if((*root)->right == NULL) {  //If the root has only left child
-
-        if( abs((*root)->left->height - (-1)) > 1)
-            balanceAVL(root);
-    }
-    else{ //If the root has only right child
-
-        if(abs((*root)->right->height - (-1)) > 1)
-            balanceAVL(root);
-    }
+    if(abs(getHeight((*root)->right) - getHeight((*root)->left)) > 1)
+        balanceAVL(root);
 
 }
 
@@ -236,9 +272,130 @@ void checkBalance(TreeNodePtr *root){ //We check each node in the tree recursive
 
 void balanceAVL(TreeNodePtr *root){
 
+    //Here we get the problematic subtree with its root
+    //First we will find the which type of rotation we need: LL, RR, LR, RL
+    //Then we will call the right rotation function
+
+
+    if(getHeight((*root)->right) > getHeight((*root)->left)){ //Our Rotation is RR or LR
+
+        if(getHeight((*root)->right->right) > getHeight((*root)->right->left))
+            rotateRR(root);
+        else
+            rotateLR(root);
+    }
+    else{ //Our Rotation is LL or RL
+
+        if(getHeight((*root)->left->left) > getHeight((*root)->left->right))
+            rotateLL(root);
+        else
+            rotateRL(root);
+    }
+
+
+    updateHeights(root); //After a rotation, we update the height of the tree
+
+}
+
+
+int getHeight(TreeNodePtr root){  //To get the NULL nodes height, we use this simple function
+    if (root != NULL)
+        return root->height;
+    else
+        return -1;
+}
+
+
+void rotateRR(TreeNodePtr *root){
+    TreeNodePtr temp;
+    copyTree((*root), &temp);
+
+    temp->right = (*root)->right->left;
+    *root = (*root)->right;
+    (*root)->left = temp;
+
+};
+
+void rotateLL(TreeNodePtr *root){
+    TreeNodePtr temp;
+    copyTree((*root), &temp);
+
+    temp->left = (*root)->left->right;
+    *root = (*root)->left;
+    (*root)->right = temp;
+
+};
 
 
 
+void rotateRL(TreeNodePtr *root){
+    int key = (*root)->left->right->data;
+    TreeNodePtr temp;
+    copyTree((*root), &temp);
+
+    temp->left = (*root)->left->right->right;
+    (*root)->left->right = (*root)->left->right->left;
+    (*root)->data = key;
+    (*root)->right = temp;
+
+};
+
+
+
+
+
+
+void rotateLR(TreeNodePtr *root){
+    int key = (*root)->right->left->data;
+    TreeNodePtr temp;
+    copyTree((*root), &temp);
+
+    temp->right = (*root)->right->left->left;
+    (*root)->right->left = (*root)->right->left->right;
+    (*root)->data = key;
+    (*root)->left = temp;
+
+};
+
+
+
+
+
+void copyTree(TreeNodePtr main, TreeNodePtr *duplicate){  //Here we duplicate a tree so that same tree with different memory locations. We need this for rotations.
+
+    if(main == NULL){
+        *duplicate = NULL;
+        return;
+    }
+
+
+    TreeNodePtr newTree = malloc(sizeof(TreeNode));
+    newTree->data = main->data;
+    newTree->height = main->height;
+    copyTree(main->right, &(newTree->right)); //Recursive
+    copyTree(main->left, &(newTree->left));
+
+    *duplicate = newTree;
+}
+
+
+//Printing a tree in 2 dimensional
+void printTree2D(TreeNodePtr tree) {//Recursive function --> RNL
+    height++; //As we go deep, we increment the height
+
+    if (tree != NULL) {
+        printTree2D(tree->right);
+
+        for(int i = 0; i < height; i++){ //Puts appropriate tabs
+            printf("\t");
+        }
+        printf("%d\n", tree->data);
+
+
+        printTree2D(tree->left);
+    }
+
+    height--;
 }
 
 
